eea87a11ba5714a5e78e72b3076d68df9c782d43
Yamuna <yamuathalasani@gmail.com>
2026-03-24 22:42:28 +0530

    changed something

---
+++ 07.spl variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#### Special Variables ####
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
