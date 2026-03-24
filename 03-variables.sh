855bc35aaadfc6d9677c7a3aea3633fcd4a0692d
Yamuna <yamuathalasani@gmail.com>
2026-03-24 20:51:08 +0530

    changed something

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Yamuna # no space between value and =
+PERSON2=Raj
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
