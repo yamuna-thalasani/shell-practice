ebec0037bdee35d19e432a34ae566c197c52b545
Yamuna <yamuathalasani@gmail.com>
2026-03-24 19:23:00 +0530

    changed something

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning Ramesh?"
+echo "Ramesh:: I am learning Shell. What about you?"
