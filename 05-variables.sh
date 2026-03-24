acb3f075ec4636884a26db9ab7f7830b01bb8dfc
Yamuna <yamuathalasani@gmail.com>
2026-03-24 21:02:22 +0530

    changed something

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please Enter your username::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
