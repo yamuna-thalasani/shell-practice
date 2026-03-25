36a07d71e3c2d3ad613f157aa43cb15d82dfc85c
Yamuna <yamuathalasani@gmail.com>
2026-03-25 14:18:55 +0530

    changed something

---
+++ 16-colours.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hello World $N, $Y I am learning Linux $N"
+echo -e "$Y Learning shell script $N, $R I am very much intrested to learn new skills $N"
