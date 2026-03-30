39b234188cdf4cb7af9b5ef865ea94df02253a48
Yamuna <yamuathalasani@gmail.com>
2026-03-30 23:33:08 +0530

    actions

---
+++ 21-script-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-1: $$"
+
+#sh 22-script-2.sh
+source ./22-script-2.sh
---
+++ 22-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-2: $$"
